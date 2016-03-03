bb908ca1209f5e9f40096fa1129744e964226218
denam60 <denam60@hotmail.fr>
2016-03-03 15:41:19 +0100

    Classes (Plateau de jeu + Graphisme)

---
+++ app/src/main/java/al22/esgi/fr/blackjackal12/Graphisme.java
@@ -0,0 +1,30 @@
+package al22.esgi.fr.blackjackal12;
+
+public class Partie
+{
+
+    public void AfficherUneCarteJoueur(int numCarte, Carte carte)
+    {
+
+    }
+
+    public void AfficherCartesDuJoueur()
+    {
+
+    }
+
+    public void AfficherUneCarteBanque(int numCarte, Carte carte)
+    {
+
+    }
+
+    public void AfficherCartesBanque()
+    {
+
+    }
+
+    public void AfficherLesPoints()
+    {
+
+    }
+}
--- app/src/main/java/al22/esgi/fr/blackjackal12/PlateauDeJeu.java
+++ app/src/main/java/al22/esgi/fr/blackjackal12/PlateauDeJeu.java
@@ -1,7 +1,28 @@
 package al22.esgi.fr.blackjackal12;
 
+import java.lang.Object;
+import java.lang.Override;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 /**
  * Created by Rawinderjeet on 03/03/2016.
  */
 public class PlateauDeJeu {
+    private int mises;
+
+    public PlateauDeJeu(){
+        ReinitialiserMise();
+    }
+
+    public void AjouterMise(int mise){
+        mises+=mise;
+    }
+
+    public void ReinitialiserMise(){
+        mises=0;
+    }
+
 }
