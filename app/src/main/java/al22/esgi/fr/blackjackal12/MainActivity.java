e1cfb4ae6ad10cf295f3ce8897aec689a37e4a73
Jeremy <jeremy.paillat@gmail.com>
2016-03-03 18:26:21 +0100

    Nouvelle interface (+ images OK)

--- app/src/main/java/al22/esgi/fr/blackjackal12/MainActivity.java
+++ app/src/main/java/al22/esgi/fr/blackjackal12/MainActivity.java
@@ -1,11 +1,12 @@
 package al22.esgi.fr.blackjackal12;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
