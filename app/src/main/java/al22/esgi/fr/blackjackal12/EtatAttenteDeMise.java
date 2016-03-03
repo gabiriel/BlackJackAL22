88838b57855896719f06ea1245d6c9589f2f9675
Tibo Mathieu <tibo.mathieu@hotmail.com>
2016-03-03 22:59:42 +0100

    Ajout des classe d'Etats de la partie

---
+++ app/src/main/java/al22/esgi/fr/blackjackal12/EtatAttenteDeMise.java
@@ -0,0 +1,21 @@
+package al22.esgi.fr.blackjackal12;
+
+import android.util.Log;
+
+/**
+ * Created by Mutaw on 03/03/2016.
+ */
+public class EtatAttenteDeMise extends EtatPartie
+{
+    @Override
+    public void ModifieEtat(Partie p) {
+        if(p == null) throw new NullPointerException("La Partie a modifier est nulle");
+        p.EtatCourant = this;
+        Afficher();
+    }
+
+    @Override
+    public void Afficher() {
+        Log.d("Etat de la Partie","En Attente de Mise");
+    }
+}
---
+++ app/src/main/java/al22/esgi/fr/blackjackal12/EtatPartie.java
@@ -0,0 +1,11 @@
+package al22.esgi.fr.blackjackal12;
+
+/**
+ * Created by Mutaw on 03/03/2016.
+ */
+public abstract class EtatPartie {
+    public abstract void  ModifieEtat(Partie p);
+    public abstract void Afficher();
+}
+
+
